2a258b92b31150aea368cacedbfd6dae7188ec43
Christian Castillo <castillochristian3@gmail.com>
2023-04-14 11:44:17 -0400

    commit tree 764c4820319d965b65b1a2053234e9d2438d434b

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,14 @@
 function reverseString(str) {
   // type your code here
+  const reversedArray = [];
+  if (str.length === 0 || str.length === 1) {
+    return str;
+  } else {
+    for (let i = str.length - 1; i >= 0; i-- ) {
+      reversedArray.push(str[i])
+    }
+  }
+  return reversedArray.join("");
 }
 
 if (require.main === module) {
@@ -16,4 +25,5 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+//takes an input of a string, starting from the last letter push each letter into a new string
 // And a written explanation of your solution
